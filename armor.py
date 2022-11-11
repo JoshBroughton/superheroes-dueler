318fb41de1662366b293fa54afaa83ebae2eea12
Josh Broughton <josh.broughton47@gmail.com>
2022-11-11 08:28:27 -0700

    completed part 2, added abilities and armor

---
+++ armor.py
@@ -0,0 +1,25 @@
+import random
+
+class Armor:
+    def __init__(self, name, max_block):
+        '''Instantiate instance properties.
+            name: String
+            max_block: Integer
+        '''
+        # TODO: Create instance variables for the values passed in.
+        self.name = name
+        self.max_block = max_block
+
+    def block(self):
+        # TODO: Return a random value between 0 and the
+        # initialized max_block strength.
+        block_amount = random.randint(0, self.max_block)
+        return block_amount
+
+
+if __name__ == "__main__":
+    # If you run this file from the terminal
+    # this block is executed.
+    armor = Armor("Debugging Shield", 10)
+    print(armor.name)
+    print(armor.block())
