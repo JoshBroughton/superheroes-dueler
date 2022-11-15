026391ab5d1d3faf967e15580337735bd8b87f98
Josh Broughton <josh.broughton47@gmail.com>
2022-11-15 13:22:51 -0700

    all done

--- hero.py
+++ hero.py
@@ -103,13 +103,6 @@ class Hero:
     def fight(self, opponent):
         ''' Current Hero will take turns fighting the opponent hero passed in.
         '''
-        # TODO: Fight each hero until a victor emerges.
-        # Phases to implement:
-        # 0) check if at least one hero has abilities. If no hero has abilities, print "Draw"
-        # 1) else, start the fighting loop until a hero has won
-        # 2) the hero (self) and their opponent must attack each other and each must take damage from the other's attack
-        # 3) After each attack, check if either the hero (self) or the opponent is alive
-        # 4) if one of them has died, print "HeroName won!" replacing HeroName with the name of the hero, and end the fight loop
         if not self.abilities and not opponent.abilities:
             print('Draw')
             return
